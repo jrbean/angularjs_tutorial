e470db1400b57778417725cc127e77e86b8c7ea7
Justin <beanjustinr@gmail.com>
2015-12-16 12:49:44 -0500

    add service and additional controller

--- app.js
+++ app.js
@@ -1,17 +1,88 @@
-angular.module('flapperNews', [])
-.controller('MainCtrl', [
+angular.module('flapperNews', ['ui.router']) 
+.config([
+'$stateProvider',
+'$urlRouteProvider',
+function($stateProvider, $urlRouteProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+    
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: 'posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouteProvider.otherwise('home');
+}])
+
+.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
+.controller('PostsCtrl', [
 '$scope',
-function($scope){
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
 
-$scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
+}])
+
+.controller('MainCtrl', [
+'$scope', 
+'posts',
+function($scope, posts) {
+
+$scope.posts = posts.posts;
+/*
+[
+    {
+      title: 'post 1', 
+      upvotes: 5
+    },
+    {
+      title: 'post 2', 
+      upvotes: 2
+    },
+    {
+      title: 'post 3', 
+      upvotes: 15
+    },
+    {
+      title: 'post 4', 
+      upvotes: 9
+    },
+    {
+      title: 'post 5', 
+      upvotes: 4
+    }
 ];
-
+*/
 $scope.addPost = function(){
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
+    if(!$scope.title || $scope.title === '') { 
+      return; 
+    }
+    $scope.posts.push({
+      title: $scope.title, 
+      link: $scope.link,
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
+    });
+    $scope.title = '';
+    $scope.link = '';
+    $scope.incrementUpvotes = function(post) {
+        post.upvotes += 1;
+    }
 };
+
 }]);
